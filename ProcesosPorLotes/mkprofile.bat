f440c505111fbf092d2c6751d7ae0c3cd7315602
nrubioc08 <150673237+nrubioc08@users.noreply.github.com>
2023-11-13 12:24:01 +0100

    mkprofile.bat

---
+++ ProcesosPorLotes/mkprofile.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Script para crear directorios con otros dentro
+REM
+REM Autor: Nicolas Rubio Cuellar
+REM Fecha: 13-11-2023
+
+mkdir %USERPROFILE%\asir %USERPROFILE%\dam %USERPROFILE%\smr %USERPROFILE%\daw %USERPROFILE%\ceti
+mkdir %USERPROFILE%\asir\docs %USERPROFILE%\asir\pract %USERPROFILE%\asir\exam 
+mkdir %USERPROFILE%\dam\docs %USERPROFILE%\dam\pract %USERPROFILE%\dam\exam
+mkdir %USERPROFILE%\smr\docs %USERPROFILE%\smr\pract %USERPROFILE%\smr\exam
+mkdir %USERPROFILE%\daw\docs %USERPROFILE%\daw\pract %USERPROFILE%\daw\exam
+mkdir %USERPROFILE%\ceti\docs %USERPROFILE%\ceti\pract %USERPROFILE%\ceti\exam
