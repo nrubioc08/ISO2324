b39a2bd6be305600589d9a8a1434824ab4f150ca
nrubioc08 <150673237+nrubioc08@users.noreply.github.com>
2023-11-14 13:53:01 +0100

    Variables.bat

---
+++ ProcesosPorLotes/Variables.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Suma de 4 variable 
+REM
+REM Autor: Nicolas Rubio Cuellar
+REM Fecha: 14-11-2023
+
+set nombre_autor=Nicolas
+echo %nombre_autor%
+set numero1=10
+set numero2=20
+set /a suma=%numero1%+%numero2%
+echo %suma%
