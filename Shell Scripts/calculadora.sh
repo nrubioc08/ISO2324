8a63ae75e5297adb643ce55b9fd23d7f00bb3430
nrubioc08 <150673237+nrubioc08@users.noreply.github.com>
2024-01-23 13:52:14 +0100

    Add files via upload

--- Shell Scripts/calculadora.sh
+++ Shell Scripts/calculadora.sh
@@ -20,32 +20,35 @@ fi
 
 i=1
 while [ $i -le $2 ]; do
+	echo
 	read -p "Operación Op1 Op2 " op op1 op2
+	echo
 	echo $op $op1 $op2
+	echo
 	case $op in
 		S)
 			suma=$(($op1+$op2))
-			echo $op1 + $op2 = $suma
+			echo $op1+$op2=$suma
 			echo $op $op1 $op2 >> $1
 		;;
 		R)
 			resta=$(($op1-$op2))
-			echo $op1 - $op2 = $resta
+			echo $op1-$op2=$resta
 			echo $op $op1 $op2 >> $1
 		;;
 		M)
 			mult=$(($op1*$op2))
-			echo $op1 x $op2 = $mult
+			echo $op1*$op2=$mult
 			echo $op $op1 $op2 >> $1
 		;;
 		D)
 			div=$(($op1/$op2))
-			echo $op1 / $op2 = $div
+			echo $op1/$op2=$div
 			echo $op $op1 $op2 >> $1
 		;;
 		X)
-			echo "Saliendo del script..."
-			exit
+			echo "Saliendo de la calculadora..."
+			break
 		;;
 		*)
 			echo "Error, operación incorrecta"
@@ -56,5 +59,8 @@ i=$(($i+1))
 done
 
 p=$(pwd)
+echo
 echo $p/$1
+echo
 cat $1
+echo
