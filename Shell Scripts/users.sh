761dce5e30fad22a4184db3ab83f558461254e51
nrubioc08 <150673237+nrubioc08@users.noreply.github.com>
2024-02-03 13:37:11 +0100

    Add files via upload

---
+++ Shell Scripts/users.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Entregable 4: recopilar info de usuarios para mostrar Usuarios y UID.
+# Autor: Nicolás Rubio Cuéllar
+# Fecha: 02-02-2024
+
+clear
+
+if [ $# -gt 1 ]; then
+	echo "Número de parámetros incorrecto"
+	exit
+fi
+
+if [ $# == 1 ]; then
+	variable=$1
+fi
+
+if [ $# == 0 ]; then
+	variable=1000
+fi
+dia=$(date +%D)
+hora=$(date +%H:%M)
+
+echo "===================================================================="
+echo
+echo "Informe de usuarios el día $dia a las $hora"
+echo
+
+pr1=$(cut -f 3 -d ":" /etc/passwd)
+usuariosContados=0
+for i in $pr1; do
+	if [ $i -ge $variable ]; then
+		usuario=$(cut -f 1,2,3,4 -d ":" /etc/passwd | grep ":$i:" | cut -f 1 -d ":")
+		echo $usuario - $i
+		echo
+		usuariosContados=$(($usuariosContados+1))
+	fi
+done
+
+
+echo
+echo "Total: $usuariosContados usuarios"
+echo
+echo "===================================================================="
+
+echo "$dia - $hora - El usuario $USER ha solicitado un informe de usuarios" >> /tmp/logeventos
